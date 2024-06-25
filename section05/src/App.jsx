ad4a86f38e7864c14b8627a7a6493db541a7b826
onlydev7777 <onlydev7777@gmail.com>
2024-06-25 18:57:20 +0900

    - props로 데이터 전달하기   - 부모 > 자식 단방향으로 전달 가능   - 자식태그는 children 이라는 매개변수로 처리 가능   - props 데이터는 object 로 받아도 되고 spread 방식으로 받아도 됨   - 가독성은 spread 방식이 더 좋아보임

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css'
-import Header from "./components/Header";
-import Main from "./components/Main.jsx";
-import Footer from "./components/Footer.jsx";
+import Button from "./components/Button.jsx";
+import Header from "./components/Header.jsx";
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3
+  }
 
   return (
       <>
-        <Header/>
-        <Main/>
-        <Footer/>
+        <Button {...buttonProps}>
+          <Header/>
+        </Button>
+
+        <Button text={"카페"}/>
+        <Button text={"블로그"}/>
       </>
   )
 }
