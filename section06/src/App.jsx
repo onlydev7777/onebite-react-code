e157ffa45da261155280b226a926d7f6f969c149
onlydev7777 <onlydev7777@gmail.com>
2024-06-26 18:19:15 +0900

    - 카운터 앱 만들기   - 기능 구현   - useState 를 App 컴포넌트에 선언     - Viewer 와 Controller 컴포넌트에 각각 props로 전달해주어야 함

--- section06/src/App.jsx
+++ section06/src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
 import Viewer from "./components/Viewer.jsx";
 import Controller from "./components/Controller.jsx";
+import {useState} from "react";
 
 function App() {
-
+  const [count, setCount] = useState(0);
+  const onClickButton = (value) => {
+    setCount(count + value);
+  }
   return (
       <div className="App">
         <h1>Simple Counter</h1>
         <section>
-          <Viewer/>
+          <Viewer count={count}/>
         </section>
         <section>
-          <Controller/>
+          <Controller onClickButton={onClickButton}/>
         </section>
       </div>
   )
