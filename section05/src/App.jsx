3ffecb9d562d4bf7f2e384ebeb75bdd2de30eb0c
onlydev7777 <onlydev7777@gmail.com>
2024-06-26 17:53:44 +0900

    - Hook 사용   1) 함수 컴포넌트, 커스텀 훅 내부에서만 호출 가능   2) 조건/반복부 에서 호출 될 수는 없다.   3) Custom Hook 을 직접 만들 수 있다.   4) Custom Hook은 함수 이름 prefix에 use가 붙으면 Custom Hook으로 인식한다.

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Register from "./components/Register.jsx";
+import HookExam from "./components/HookExam.jsx";
 
 function App() {
 
   return (
       <>
-        <Register/>
+        <HookExam/>
       </>
   )
 }
