31db8de4e507d3837ccde6b6048979314f636efc
onlydev7777 <onlydev7777@gmail.com>
2024-06-27 17:12:55 +0900

    - TODO List 프로젝트   1) useReducer 테스트

---
+++ section09/src/components/Editor.jsx
@@ -0,0 +1,36 @@
+import "./Editor.css";
+import {useState, useRef} from "react";
+
+const Editor = ({onCreate}) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  }
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  }
+
+  const onKeyDownContent = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  }
+  return (
+      <div className="Editor">
+        <input value={content} onChange={onChangeContent}
+               onKeyDown={onKeyDownContent}
+               ref={contentRef}
+               placeholder="새로운 Todo..."/>
+        <button onClick={onSubmit}>추가</button>
+      </div>
+  );
+};
+
+export default Editor;
