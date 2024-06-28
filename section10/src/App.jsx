e491d1ca30d6584b4ca2286ab089aa0b6d40e3fd
onlydev7777 <onlydev7777@gmail.com>
2024-06-28 16:15:32 +0900

    - TODO List 프로젝트   1) useMemo 최적화 프로젝트 생성

---
+++ section10/src/App.jsx
@@ -0,0 +1,86 @@
+import './App.css'
+import Header from "./components/Header.jsx";
+import Editor from "./components/Editor.jsx";
+import List from "./components/List.jsx";
+import {useState, useRef, useReducer} from "react";
+import Exam from "./components/Exam.jsx";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "집에가기",
+    date: new Date().getTime(),
+  }
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((todo) =>
+          todo.id === action.targetId
+              ? {...todo, isDone: !todo.isDone}
+              : todo
+      );
+    case 'DELETE':
+      return state.filter((todo) =>
+          todo.id !== action.targetId
+      );
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3)
+  const onCreate = (content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime()
+      }
+    });
+  }
+
+  const onUpdate = (targetId) => {
+    dispatch({
+      type: 'UPDATE',
+      targetId: targetId
+    });
+  }
+
+  const onDelete = (targetId) => {
+    dispatch({
+      type: 'DELETE',
+      targetId: targetId
+    });
+  }
+
+  return (
+      <div className="App">
+        <Header/>
+        <Editor onCreate={onCreate}/>
+        <List todos={state} onUpdate={onUpdate} onDelete={onDelete}/>
+      </div>
+  )
+}
+
+export default App
---
+++ section10/src/components/TodoItem.jsx
@@ -0,0 +1,22 @@
+import "./TodoItem.css";
+
+const TodoItem = ({id, content, date, isDone, onUpdate, onDelete}) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  }
+
+  const onDeleteButton = () => {
+    onDelete(id);
+  }
+
+  return (
+      <div className="TodoItem">
+        <input type="checkbox" checked={isDone} onChange={onChangeCheckbox}/>
+        <div className="content">{content}</div>
+        <div className="date">{new Date(date).toLocaleDateString()}</div>
+        <button onClick={onDeleteButton}>삭제</button>
+      </div>
+  );
+};
+
+export default TodoItem;
