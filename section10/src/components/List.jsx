49d02656ebfbc08adbfed5a1da50e6ad166b0f11
onlydev7777 <onlydev7777@gmail.com>
2024-06-28 16:30:53 +0900

    - TODO List 프로젝트   1) useMemo 없이 리렌더링 될 때 마다 getAnalyzedData() 호출.   >> 검색어 입력할 때에도 리렌더링 되는데 성능저하 유발

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,6 +1,6 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-import {useState} from "react";
+import {useState, useMemo} from "react";
 
 const List = ({todos, onUpdate, onDelete}) => {
   const [search, setSearch] = useState("");
@@ -22,9 +22,25 @@ const List = ({todos, onUpdate, onDelete}) => {
 
   const filteredTodos = getFilteredData();
 
+  const getAnalyzedData = () => {
+    console.log("getAnalyzedData!!");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return {totalCount, doneCount, notDoneCount};
+  }
+
+  const {totalCount, doneCount, notDoneCount} = getAnalyzedData();
+
   return (
       <div className="List">
         <h4>Todo List 🌱</h4>
+        <div>
+          <div>total : {totalCount}</div>
+          <div>done : {doneCount}</div>
+          <div>notDone : {notDoneCount}</div>
+        </div>
         <input value={search} onChange={onChangeSearch}
                placeholder="검색어를 입력하세요"/>
         <div className="todos_wrapper">
