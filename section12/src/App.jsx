9fbd8a4b2265cfe089bb155b2f76656a0adacd4e
onlydev7777 <onlydev7777@gmail.com>
2024-07-01 18:26:51 +0900

    - 감정 일기장 프로젝트   1) Diary.jsx > useParams 사용      : /diary/:id 와 같이 일기의 상세 id 주소를 렌더링 할 때 선언      : /diary/1 요청 시 {params.id}에 '1'이 담겨있음
      2) Home.jsx > useSearchParams 사용
         : query 파라미터로 넘어올 경우 key/value 형태로 처리
         : /?value=kkkk 일 경우 params.get("value") 로 출력 가능

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -19,13 +19,13 @@ function App() {
         <div>
           <Link to={"/"}>Home</Link>
           <Link to={"/new"}>New</Link>
-          <Link to={"/diary"}>Diary</Link>
+          <Link to={"/diary/1"}>Diary</Link>
         </div>
         <button onClick={onClickButton}>New 페이지로 이동</button>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/new" element={<New/>}/>
-          <Route path="/diary" element={<Diary/>}/>
+          <Route path="/diary/:id" element={<Diary/>}/>
           <Route path="*" element={<NotFound/>}/>
         </Routes>
       </>
