2b395284a87474d164e4795b2f351a25b36fa777
onlydev7777 <onlydev7777@gmail.com>
2024-07-01 18:18:30 +0900

    - 감정 일기장 프로젝트   1) Link 사용      - SPA 방식으로 페이지 load
      2) useNavigate 사용
         - 버튼 onClick 처리 시 함수 호출로 SPA 방식 페이지 load

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,7 +1,34 @@
+import {Routes, Route, Link, useNavigate} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import New from "./pages/New.jsx";
+import Diary from "./pages/Diary.jsx";
+import NotFound from "./pages/NotFound.jsx";
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new")
+  };
 
   return (
-      <div>Hello!</div>
+      <>
+        <div>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/new"}>New</Link>
+          <Link to={"/diary"}>Diary</Link>
+        </div>
+        <button onClick={onClickButton}>New 페이지로 이동</button>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/new" element={<New/>}/>
+          <Route path="/diary" element={<Diary/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </>
   )
 }
 
