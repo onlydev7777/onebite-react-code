55840581752228d8d9946b6892cf99b27ea6861e
onlydev7777 <onlydev7777@gmail.com>
2024-07-03 17:08:47 +0900

    - 감정 일기장 프로젝트   1) useReducer 사용

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,11 +4,38 @@ import New from "./pages/New.jsx";
 import Diary from "./pages/Diary.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Edit from "./pages/Edit.jsx";
+import {useReducer} from "react";
+
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용"
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용"
+  },
+  {
+    id: 3,
+    createdDate: new Date().getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용"
+  },
+]
+
+function reducer({state, dispatch}) {
+  return state;
+}
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
   return (
       <>
         <Routes>
