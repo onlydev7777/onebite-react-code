d423a81ea5909ff6a7b4d175f348b278c0aa9b80
onlydev7777 <onlydev7777@gmail.com>
2024-07-04 17:06:40 +0900

    - 감정 일기장 프로젝트   1) Editor.jsx      - useState 사용        : 감정 일기장 입력 폼 state 관리
         - EmotionItem 컴포넌트에 onChangeInput 메서드 props 전달
           : 컴포넌트 클릭 이벤트 이므로 e.target 객체 정의해서 전달
    
         - 작성완료 버튼 기능 2개 이므로 props 전달 받아서 처리
           : New 페이지 에서는 onCreate
           : Edit 페이지 에서는 onUpdate
    
      2) EmotionItem.jsx
         - onClick Props 전달 받아서 onClick 이벤트 처리
    
      3) New.jsx
         - useContext 사용
           : App.jsx 의 DiaryDispatchContext.onCreate 메서드 get
    
         - Editor 컴포넌트의 props로 전달할 onSubmit 정의
           : DiaryDispatchContext.onCreate 메서드 호출
    
         - useNavigate 사용
           : 작성완료 후 뒤로가기 방지 처리
           : nav(-1, {replace : true});

--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,6 +1,8 @@
 import "./Editor.css"
 import EmotionItem from "./EmotionItem.jsx";
 import Button from "./Button.jsx";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 const emotionList = [
   {
@@ -25,14 +27,53 @@ const emotionList = [
   }
 ];
 
-const Editor = () => {
-  const emotionId = 1;
+const getStringedDate = (targetDate) => {
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = "0" + month;
+  }
+
+  if (date < 10) {
+    date = "0" + date;
+  }
+
+  return year + "-" + month + "-" + date;
+}
+const Editor = ({onSubmit}) => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: ""
+  });
+  const nav = useNavigate();
+
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value
+    });
+  };
+
+  const onClickSubmitButton = () => {
+    onSubmit(input);
+  }
 
   return (
       <div className="Editor">
         <section className="date_section">
           <h4>오늘의 날짜</h4>
-          <input type="date"/>
+          <input name="createdDate" onChange={onChangeInput} type="date"
+                 value={getStringedDate(input.createdDate)}/>
         </section>
         <section className="emotion_section">
           <h4>오늘의 감정</h4>
@@ -40,17 +81,27 @@ const Editor = () => {
             {emotionList.map((item) =>
                 <EmotionItem key={item.emotionId}
                              {...item}
-                             isSelected={item.emotionId === emotionId}/>
+                             isSelected={item.emotionId === input.emotionId}
+                             onClick={() => onChangeInput({
+                               target: {
+                                 name: "emotionId",
+                                 value: item.emotionId
+                               }
+                             })}
+                />
             )}
           </div>
         </section>
         <section className="content_section">
           <h4>오늘의 일기</h4>
-          <textarea placeholder="오늘은 어떘나요?"/>
+          <textarea name="content" value={input.content}
+                    onChange={onChangeInput}
+                    placeholder="오늘은 어떘나요?"/>
         </section>
         <section className="button_section">
-          <Button text={"취소하기"}/>
-          <Button text={"작성완료"} type={"POSITIVE"}/>
+          <Button text={"취소하기"} onCLick={() => nav(-1)}/>
+          <Button text={"작성완료"} type={"POSITIVE"}
+                  onCLick={onClickSubmitButton}/>
         </section>
       </div>
   );
--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,17 +1,29 @@
 import Header from "../components/Header.jsx";
 import Button from "../components/Button.jsx";
 import Editor from "../components/Editor.jsx";
+import {useNavigate} from "react-router-dom";
+import {useContext} from "react";
+import {DiaryDispatchContext} from "../App.jsx";
 
 const New = () => {
+  const nav = useNavigate();
+  const {onCreate} = useContext(DiaryDispatchContext);
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", {replace: true});
+  }
+
   return (
       <div>
         <div>
           <Header title={"새 일기 쓰기"}
-                  leftChild={<Button text={"< 뒤로가기"}/>}
+                  leftChild={<Button text={"< 뒤로가기"}
+                                     onCLick={() => nav(-1)}/>
+                  }
           />
         </div>
         <div>
-          <Editor/>
+          <Editor onSubmit={onSubmit}/>
         </div>
       </div>
   );
