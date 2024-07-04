b1f54b58508c2c2c86323f2bd504cb018e55658e
onlydev7777 <onlydev7777@gmail.com>
2024-07-04 16:24:33 +0900

    - 감정 일기장 프로젝트   1) Editor.jsx      - 작성하기 컴포넌트 작성
      2) EmotionItem.jsx
         - 작성하기 > EmotionItem 컴포넌트 작성

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,5 +1,20 @@
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
+
 const New = () => {
-  return <div>New</div>
+  return (
+      <div>
+        <div>
+          <Header title={"새 일기 쓰기"}
+                  leftChild={<Button text={"< 뒤로가기"}/>}
+          />
+        </div>
+        <div>
+          <Editor/>
+        </div>
+      </div>
+  );
 }
 
 export default New
