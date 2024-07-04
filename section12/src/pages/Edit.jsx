d479c1b8f0e7c89ef89e5393d6fe945837b56c4c
onlydev7777 <onlydev7777@gmail.com>
2024-07-04 17:42:07 +0900

    - 감정 일기장 프로젝트
      1) Edit.jsx
         - useContext 사용
           : DiaryStateContext.data
             - 기존 입력 값 세팅
           : DiaryDispatchContext.onUpdate
             - 사용자 입력 값 수정
           : DiaryDispatchContext.onDelete
             - 해당 일기 삭제
    
         - useEffect 사용
           : 정상 접근일 경우 DiaryStateContext.data 에서 findKey 로 찾아서 state 값 저장
           : 잘못된 접근일 경우 뒤로가기 처리
             => useNavigate 는 컴포넌트가 다 렌더링 된 이후에 처리 될 수 있으므로 반드시 useState 로 처리 해주어야 함
    
      2) Editor.jsx
         - Edit 페이지 컴포넌트로 부터 props 로 initData 받는다.
    
         - useEffect 사용
           : initData 가 있을 경우 기존 입력 값 세팅 처리

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,10 +1,53 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
+import {useContext, useEffect, useState} from "react";
+import {DiaryDispatchContext, DiaryStateContext} from "../App.jsx";
 
 const Edit = () => {
   const params = useParams();
+  const nav = useNavigate();
+  const [curDiaryItem, setCurDiaryItem] = useState();
+  const {onDelete, onUpdate} = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    const currentDiaryItem = data.find(
+        (item) => String(item.id) === String(params.id));
+
+    if (!currentDiaryItem) {
+      window.alert("존재하지 않는 일기입니다.");
+      nav("/", {replace: true});
+    }
+
+    setCurDiaryItem(currentDiaryItem);
+  }, [params.id, data]);
+
+  const onClickDelete = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onDelete(params.id);
+      nav("/", {replace: true});
+    }
+  }
+
+  const onSubmit = (input) => {
+    if (window.confirm("정말 수정하시겠습니까?")) {
+      onUpdate(params.id, input.createdDate.getTime(), input.emotionId,
+          input.content);
+      nav("/", {replace: true});
+    }
+  }
 
   return (
-      <div>{params.id} 번째 일기 수정</div>
+      <div>
+        <Header title={"일기 수정하기"}
+                leftChild={<Button text={"< 뒤로가기"} onCLick={() => nav(-1)}/>}
+                rightChild={<Button text={"삭제하기"} type={"NEGATIVE"}
+                                    onCLick={() => onClickDelete()}/>}
+        />
+        <Editor initData={curDiaryItem} onSubmit={onSubmit}/>
+      </div>
   );
 };
 
